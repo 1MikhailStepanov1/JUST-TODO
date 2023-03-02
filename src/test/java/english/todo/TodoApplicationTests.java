43075c3c217990f2c60573cfaa344348113c813f
Valerii Butorin <butorin.v.a@mail.ru>
2023-03-02 18:14:38 +0300

    init

---
+++ src/test/java/english/todo/TodoApplicationTests.java
@@ -0,0 +1,17 @@
+package english.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        assertEquals(1, 1);
+    }
+
+}
